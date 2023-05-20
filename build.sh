29ad714bf5d482b1ae019880a04d6bf939cda25f
Ubuntu <ubuntu@ip-172-31-90-152.ec2.internal>
2023-05-20 21:17:19 +0000

    first commit

---
+++ build.sh
@@ -0,0 +1,9 @@
+build.sh
+#!/bin/bash
+
+# Build Docker image from Dockerfile
+docker build -t "reactapp:$IMAGE_VERSION" .
+echo "Docker image build completed successfully."
+
+# Display list of Docker images
+docker images
