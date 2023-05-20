6052854a090c23ca6e1b91adf4cda743b042e129
Ubuntu <ubuntu@ip-172-31-90-152.ec2.internal>
2023-05-20 22:10:32 +0000

    modify build.sh v1

--- build.sh
+++ build.sh
@@ -2,8 +2,9 @@ build.sh
 #!/bin/bash
 
 # Build Docker image from Dockerfile
-docker build -t "reactapp:$IMAGE_VERSION" .
+docker build -t reactapp:$IMAGE_VERSION .
 echo "Docker image build completed successfully."
 
 # Display list of Docker images
 docker images
+
